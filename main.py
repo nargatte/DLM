31ac8c57f4eab039961646a5b4606ff72dc90d90
Marcin Galiński <marcinggalinski@gmail.com>
2023-03-13 19:54:43 +0100

    data loading

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from tensorflow.keras import datasets, layers, models
+
+
+classes = {
+    0: "airplane",
+    1: "automobile",
+    2: "bird",
+    3: "cat",
+    4: "deer",
+    5: "dog",
+    6: "frog",
+    7: "horse",
+    8: "ship",
+    9: "truck"
+}
+
+
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
