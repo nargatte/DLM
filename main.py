0a573adc5864f2e9be3bbf03ae0e771caadd9d6c
Marcin Galiński <marcinggalinski@gmail.com>
2023-03-16 14:57:49 +0100

    simple network with only conv and linear layers

--- main.py
+++ main.py
@@ -16,14 +16,19 @@ class Net(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
+        self.conv3 = nn.Conv2d(16, 32, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        # self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc1 = nn.Linear(32 * 20 * 20, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        # x = self.pool(F.relu(self.conv1(x)))
+        # x = self.pool(F.relu(self.conv2(x)))
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -60,10 +65,10 @@ transform = transforms.Compose([
 cifar10_data = datasets.CIFAR10("./cifar10", download=True, transform=transform)
 data_loader = torch.utils.data.DataLoader(cifar10_data, shuffle=True, batch_size=batch_size)
 
-for images, labels in data_loader:
-    imshow(torchvision.utils.make_grid(images))
-    print(' '.join(f'{classes[int(labels[j])]:5s}' for j in range(batch_size)))
-    break
+# for images, labels in data_loader:
+#     imshow(torchvision.utils.make_grid(images))
+#     print(' '.join(f'{classes[int(labels[j])]:5s}' for j in range(batch_size)))
+#     break
 
 net = Net()
 
