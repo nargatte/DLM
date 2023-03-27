64ffdf61fe75ffd1f84f9c74ca9218edabef6383
Marcin Galiński <marcinggalinski@gmail.com>
2023-03-27 21:55:46 +0200

    inception modules

--- Projekt 1/networks.py
+++ Projekt 1/networks.py
@@ -60,7 +60,6 @@ class PoolingNet(nn.Module):
 
 
 class ResidualNet(nn.Module):
-
     def __init__(self):
         super().__init__()
         self.conv1 = ResidualNet._conv_block(3, 64, True)
@@ -94,3 +93,75 @@ class ResidualNet(nn.Module):
         if pool:
             layers.append(nn.MaxPool2d(2))
         return nn.Sequential(*layers)
+
+
+# based on https://pytorch.org/vision/main/_modules/torchvision/models/inception.html
+class InceptionNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = InceptionNet._conv_block(3, 32, kernel_size=3)
+        self.conv2 = InceptionNet._conv_block(32, 32, kernel_size=3)
+        self.conv3 = InceptionNet._conv_block(32, 64, kernel_size=3)
+
+        self.inc1 = InceptionNet.Inception(64, 32)
+        self.inc2 = InceptionNet.Inception(256, 64)
+        self.inc3 = InceptionNet.Inception(288, 64)
+
+        self.fc1 = nn.Linear(288 * 26 * 26, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 64)
+        self.fc5 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+
+        x = self.inc1(x)
+        x = self.inc2(x)
+        x = self.inc3(x)
+
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
+        x = self.fc5(x)
+        return x
+
+
+    class Inception(nn.Module):
+        def __init__(self, in_channels, pool_features):
+            super().__init__()
+            self.branch_1x1 = InceptionNet._conv_block(in_channels, 64, kernel_size=1)
+
+            self.branch_5x5 = nn.Sequential(
+                InceptionNet._conv_block(in_channels, 48, kernel_size=1),
+                InceptionNet._conv_block(48, 64, kernel_size=5, padding=2)
+            )
+
+            self.branch_3x3 = nn.Sequential(
+                InceptionNet._conv_block(in_channels, 64, kernel_size=1),
+                InceptionNet._conv_block(64, 96, kernel_size=3, padding=1),
+            )
+
+            self.branch_pool = nn.Sequential(
+                nn.MaxPool2d(3, stride=1, padding=1),
+                InceptionNet._conv_block(in_channels, pool_features, kernel_size=1)
+            )
+
+        def forward(self, x):
+            branch_1x1 = self.branch_1x1(x)
+            branch_5x5 = self.branch_5x5(x)
+            branch_3x3 = self.branch_3x3(x)
+            branch_pool = self.branch_pool(x)
+            return torch.cat((branch_1x1, branch_5x5, branch_3x3, branch_pool), 1)
+
+    @staticmethod
+    def _conv_block(in_channels, out_channels, **kwargs):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, **kwargs),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
